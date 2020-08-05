5548d1d9f6a4e34f19e000fde35f6ed7e8edb1ce
werliton <werliton.carlos@ceuma.br>
2020-08-04 22:38:01 -0300

    Criando card de teachers

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './styles.css'
 import PageHeader from '../../components/PageHeader'
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
@@ -21,6 +23,31 @@ export default function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/4674324?s=460&u=cb676169391ac204b824569fd7465fa36488624d&v=4" alt="Perfil"/>
+                        <div>
+                            <strong>Werliton Silva</strong>
+                            <span>Programação Front-End</span>
+                        </div>
+                    </header>
+                    <p>
+                    Systems Analist at Grupo Ceuma works with React, React Native, Firebase, PHP, Moodle, JS lover and constantly bothered. 
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 150,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
