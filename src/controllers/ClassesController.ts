4e41e093beaabe01d7990eeccd4ce4181ee8b435
werliton <werliton.carlos@ceuma.br>
2020-08-08 19:51:07 -0300

    Criando o primeiro controller para classes

---
+++ src/controllers/ClassesController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express'
+import db from '../database/connection'
+import convertHourToMinutes from '../utils/convertHourToMinutes'
+
+interface ScheduleItem{
+    week_day: number
+    from: string
+    to: string
+}
+
+export default class ClassesControler{
+    async create(request:Request, response: Response){
+
+        const {
+            name,
+            avatar,
+            whatsapp,
+            bio,
+            subject,
+            cost,
+            schedule
+        } = request.body
+   
+        const trx = await db.transaction()
+   
+        try {
+           const insertedUsersIds = await trx('users').insert({
+               name,
+               avatar, 
+               whatsapp,
+               bio
+           })
+      
+           const user_id = insertedUsersIds[0]
+      
+           const insertedClassesIds = await trx('classes').insert({
+               subject,
+               cost,
+               user_id
+           })
+      
+           const class_id = insertedClassesIds[0]
+      
+           const classSchedule = await schedule.map((item: ScheduleItem) => ({
+              week_day: item.week_day,
+              from: convertHourToMinutes(item.from),
+              to: convertHourToMinutes(item.to),
+              class_id
+           }))
+      
+           await trx('class_schedule').insert(classSchedule)
+      
+           trx.commit()
+      
+           console.log('Usuário inserido com sucesso.');
+      
+           return response.status(201).send()
+        } catch (error) {
+   
+           await trx.rollback()
+   
+            return response.status(400).json({
+                error: 'Unexpected error while creating new class'
+            })
+        }
+   }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,71 +1,11 @@
 import express, { request } from 'express'
-import db from './database/connection'
-import convertHourToMinutes from './utils/convertHourToMinutes'
+import ClassesControler from './controllers/ClassesController'
 
 const routes = express.Router()
 
-interface ScheduleItem{
-    week_day: number
-    from: string
-    to: string
-}
+const classesController = new ClassesControler()
 
-routes.post('/classes', async (request, response) => {
-
-    
-     const {
-         name,
-         avatar,
-         whatsapp,
-         bio,
-         subject,
-         cost,
-         schedule
-     } = request.body
-
-     const trx = await db.transaction()
-
-     try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar, 
-            whatsapp,
-            bio
-        })
-   
-        const user_id = insertedUsersIds[0]
-   
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        })
-   
-        const class_id = insertedClassesIds[0]
-   
-        const classSchedule = await schedule.map((item: ScheduleItem) => ({
-           week_day: item.week_day,
-           from: convertHourToMinutes(item.from),
-           to: convertHourToMinutes(item.to),
-           class_id
-        }))
-   
-        await trx('class_schedule').insert(classSchedule)
-   
-        trx.commit()
-   
-        console.log('Usuário inserido com sucesso.');
-   
-        return response.status(201).send()
-     } catch (error) {
-
-        await trx.rollback()
-
-         return response.status(400).json({
-             error: 'Unexpected error while creating new class'
-         })
-     }
-})
+routes.post('/classes', classesController.create)
 
 
 
