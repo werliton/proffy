78a411466bcb710b490353bc994694e0e7eccf4b
werliton <werliton.carlos@ceuma.br>
2020-08-07 21:03:41 -0300

    Criando primeira rota e criando o readme

---
+++ REAME.md
@@ -0,0 +1,13 @@
+# Funcionalidades
+
+## Conexoes
+
+- Rota para listar o total de conexões realizadas
+- Rota para criar uma nova conexao
+
+
+## Aula
+- Rota para criar uma aula
+- Rota Listar aulas
+    - Filtrar por matérias, dias da semana e horário
+
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('oi2');
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', (resquest, response) => {
+    return response.json({
+        nome:'letox',
+        age:20
+    })
+})
+
+
+
+app.listen(3333)
+
