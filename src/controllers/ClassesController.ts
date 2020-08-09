7d21666109e1af033134427d97799fa5b0344ed6
werliton <werliton.carlos@ceuma.br>
2020-08-09 10:35:39 -0300

    Finalizando as querys por filtros

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -32,7 +32,8 @@ export default class ClassesControler{
                 .from('class_schedule')
                 .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                 .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                
+                .whereRaw('`class_schedule`.`from` <= ?? ', timeInMinutes)
+                .whereRaw('`class_schedule`.`to` > ?? ', timeInMinutes)
             })
             .where('classes.subject', '=', subject)
             .join('users', 'classes.user_id', '=', 'users.id')
