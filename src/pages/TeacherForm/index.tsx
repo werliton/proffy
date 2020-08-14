705e57b2a0512145c9fe2be98b0b3bfceeb6062f
werliton <werliton.carlos@ceuma.br>
2020-08-13 23:59:05 -0300

    Criando componete select

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -6,13 +6,22 @@ import './styles.css'
 import warningIcon from '../../assets/images/icons/warning.svg'
 import Button from '../../components/Button'
 import TextAreaBlock from '../../components/TextareaBlock'
+import SelectBlock from '../../components/Select'
+
+const subjects = [
+    { value: 'Artes', label: 'Artes'},
+    { value: 'Biologia', label: 'Biologia'},
+    { value: 'Matematica', label: 'Matematica'},
+    { value: 'Fisica', label: 'Fisica'},
+    { value: 'História', label: 'História'}
+]
 
 export default function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container" >
-            <PageHeader 
-            title="Que incrível você quer dá aulas."
-            description="O primeiro passo é preecher esse formulário de inscrição"
+            <PageHeader
+                title="Que incrível você quer dá aulas."
+                description="O primeiro passo é preecher esse formulário de inscrição"
             />
 
             <main>
@@ -28,13 +37,17 @@ export default function TeacherForm() {
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                    <InputBlock id="subject" label="Matéria" />
+                    <SelectBlock
+                        id="subject"
+                        label="Matéria"
+                        options={subjects}
+                    />
                     <InputBlock id="cost" label="Custo da hora aula" />
                 </fieldset>
 
                 <footer>
                     <p>
-                    <img src={warningIcon} alt="Aviso"/>
+                        <img src={warningIcon} alt="Aviso" />
                     Importante! <br />
                     Preencha todos os dados
                     </p>
