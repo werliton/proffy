083b8bf51efee93fab7bfac6960547310204b833
werliton <werliton.carlos@ceuma.br>
2020-08-14 15:22:37 -0300

    Criando services e conectando com a api

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
@@ -7,8 +7,20 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
 
 export default function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('connections')
+        .then(response => {
+            const { total } = response.data
+            setTotalConnections(total)
+        })
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -35,7 +47,7 @@ export default function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexoes realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                    Total de { totalConnections } conexoes realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
                 </span>
             </div>
         </div>
