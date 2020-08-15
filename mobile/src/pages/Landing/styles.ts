2ab4fb621bcbcc29e6f9e642b386437fefc8f77c
werliton <werliton.carlos@ceuma.br>
2020-08-15 14:53:42 -0300

    Install e config font customizadas

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -8,7 +8,18 @@ const styles = StyleSheet.create({
         padding: 40 
     },
     banner:{
-        width: '100%'
+        width: '100%',
+        resizeMode: 'contain'
+    },
+    title:{
+        fontFamily: 'Poppins_400Regular',
+        color: '#FFF',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80
+    },
+    titleBold: {
+        fontFamily:'Poppins_600SemiBold'
     }
 })
 
