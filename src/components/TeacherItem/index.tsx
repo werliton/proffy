9e445eb4621afbaeeb664db1a55a1a4eecaea084
werliton <werliton.carlos@ceuma.br>
2020-08-15 00:48:04 -0300

    Listando teachers no teacherlist

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,30 +4,46 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 import './styles.css'
 import Button from '../Button';
 
-export default function TeacherItem() {
+interface TeacherItemProps {
+    listTeachers: Array<{
+        subject: string
+        cost: number,
+        name: string,
+        avatar: string
+        whatsapp: string,
+        bio: string
+    }>
+}
+
+export default function TeacherItem({ listTeachers }: TeacherItemProps) {
     return (
         <main>
-            <article className="teacher-item">
-                <header>
-                    <img src="https://avatars1.githubusercontent.com/u/4674324?s=460&u=cb676169391ac204b824569fd7465fa36488624d&v=4" alt="Perfil" />
-                    <div>
-                        <strong>Werliton Silva</strong>
-                        <span>Programação Front-End</span>
-                    </div>
-                </header>
-                <p>
-                    Systems Analist at Grupo Ceuma works with React, React Native, Firebase, PHP, Moodle, JS lover and constantly bothered.
+            {
+                listTeachers.map((item, index) => (
+                    <article className="teacher-item">
+                        <header>
+                            <img src={item.avatar} alt="Perfil" />
+                            <div>
+                                <strong>{item.name}</strong>
+                                <span>{item.subject}</span>
+                            </div>
+                        </header>
+                        <p>
+                            {item.bio}
                     </p>
-                <footer>
-                    <p>
-                        Preço/hora
-                            <strong>R$ 150,00</strong>
-                    </p>
-                    <Button title="Entrar em contato">
-                        <img src={whatsappIcon} alt="whatsapp" />
-                    </Button>
-                </footer>
-            </article>
+                        <footer>
+                            <p>
+                                Preço/hora
+                            <strong>R$ {item.cost},00</strong>
+                            </p>
+                            <Button title="Entrar em contato">
+                                <img src={whatsappIcon} alt="whatsapp" />
+                            </Button>
+                        </footer>
+                    </article>
+
+                ))
+            }
         </main>
     )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,31 +1,61 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import './styles.css'
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 import InputBlock from '../../components/InputBlock'
 import SelectBlock from '../../components/Select'
 import { subjects, week_day } from '../../utils/constantes'
+import api from '../../services/api'
+import Button from '../../components/Button'
 
 export default function TeacherList() {
+    const [subject, setSubject] = useState('')
+    const [weekDay, setWeekDay] = useState('')
+    const [time, setTime] = useState('')
+    const [listTeachers, setListTeachers] = useState([])
+    
+    function seachTeachers(e:FormEvent) {
+        e.preventDefault()
+        api.get('classes',{
+            params:{
+                subject,
+                week_day:weekDay,
+                time
+            }
+        })
+        .then(response => {
+            setListTeachers(response.data)
+        })
+        .catch(()=> alert('Erro ao buscar profs'))
+    }
+
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <form action="" id="search-teachers">
+                <form id="search-teachers" onSubmit={seachTeachers}>
                     <SelectBlock
                         id="subject"
                         label="Matéria"
+                        value={subject}
+                        onChange={e => setSubject(e.target.value)}
                         options={subjects}
                     />
                     <SelectBlock
                         id="week_day"
                         label="Dia da semana"
+                        value={weekDay}
+                        onChange={e => setWeekDay(e.target.value)}
                         options={week_day}
                     />
-                    <InputBlock type="time" label="Horário" id="hour"/>
+                    <InputBlock type="time" label="Horário" id="hour" value={time}
+                        onChange={e => setTime(e.target.value)}/>
+
+                        <Button name="Buscar" title="Buscar" type="submit"/>
                 </form>
             </PageHeader>
 
-            <TeacherItem />
+            <TeacherItem listTeachers={listTeachers}/>
         </div>
     )
 }
