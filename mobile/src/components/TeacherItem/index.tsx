d479a8e6100fb64e9c85c4ae7f9b04bf5c6980a3
werliton <werliton.carlos@ceuma.br>
2020-08-18 12:58:24 -0300

    Salvando as conexoes

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,6 +8,7 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles'
 import AsyncStorage from '@react-native-community/async-storage'
+import api from '../../services/api'
 
 export interface Teacher {
     id: number
@@ -28,6 +29,9 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
     const [isFavorited, setIsFavorited] = useState(favorited)
 
     function handleLinkTowhatsapp() {
+        api.post('connections',{
+            user_id: teacher.id
+        })
         Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
     }
 
