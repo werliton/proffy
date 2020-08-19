ed413f085bc78153514e96346cc3c800b2f56ea8
werliton <werliton.carlos@ceuma.br>
2020-08-19 11:38:40 -0300

    Preparando nossa aplicação

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -30,10 +30,10 @@ export default class ClassesControler{
             .whereExists(function() {
                 this.select('class_schedule.*')
                 .from('class_schedule')
-                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                .whereRaw('`class_schedule`.`from` <= ?? ', timeInMinutes)
-                .whereRaw('`class_schedule`.`to` > ?? ', timeInMinutes)
+                .whereRaw('class_schedule.class_id = classes.id')
+                .whereRaw('class_schedule.week_day = ??', [Number(week_day)])
+                .whereRaw('class_schedule.from <= ?? ', timeInMinutes)
+                .whereRaw('class_schedule.to > ?? ', timeInMinutes)
             })
             .where('classes.subject', '=', subject)
             .join('users', 'classes.user_id', '=', 'users.id')
@@ -61,7 +61,7 @@ export default class ClassesControler{
                avatar, 
                whatsapp,
                bio
-           })
+           }).returning('id')
       
            const user_id = insertedUsersIds[0]
       
@@ -69,7 +69,7 @@ export default class ClassesControler{
                subject,
                cost,
                user_id
-           })
+           }).returning('id')
       
            const class_id = insertedClassesIds[0]
       
