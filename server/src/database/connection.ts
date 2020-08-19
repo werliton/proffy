8c9d55198f39dbd0c2221116f7a638b05845125e
werliton <werliton.carlos@ceuma.br>
2020-08-19 14:27:54 -0300

    Configurações avançadas

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -4,13 +4,12 @@ import 'dotenv/config'
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'docker',
-        database: 'proffy'
+        host: process.env.PG_HOST,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE
     },
-    useNullAsDefault: true,
-
+    useNullAsDefault: true
 })
 
 export default db
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,8 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.listen(process.env.PORT || 3333)
 
-console.log('Servidor rodando na porta 3333');
-
-app.listen(3333)
-
+console.log(`Servidor rodando na porta ${process.env.PORT}`);
